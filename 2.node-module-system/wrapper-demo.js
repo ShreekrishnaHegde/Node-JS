eb46176b4cad99f8a006c4d9068b062cc220c437
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-11-27 01:54:26 +0530

    wrapping-things

---
+++ 2.node-module-system/wrapper-demo.js
@@ -0,0 +1,8 @@
+
+const wrapperExplorer=require(`./wrapper-explorer`);
+console.log(`in wrapper-demo.js file`);
+
+console.log(`_filename`,__filename);
+console.log(`dirname`,__dirname);
+
+wrapperExplorer.greet("Krishna");
