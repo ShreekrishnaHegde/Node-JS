05c67850942b96ff7ee2a96dbffe03f91f8e5b9f
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-11-26 10:01:25 +0530

    Hello World in js

---
+++ hello-world-node/index.js
@@ -0,0 +1,9 @@
+console.log("Hey JS");
+
+const arr=[1,2,3,4,5];
+console.log(arr);
+setTimeout(()=>{
+    console.log(`This message is delayed by 2000ms`)
+},2000)
+
+console.log(`This is the last line of sync code`);
